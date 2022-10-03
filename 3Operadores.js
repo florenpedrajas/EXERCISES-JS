ad9010d4244a916cd5130184c1cf2d718751e2d5
FlorenP <fpedrajasm3@gmail.com>
2022-10-03 22:50:17 +0200

    first commit

---
+++ 3Operadores.js
@@ -0,0 +1,26 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+var multiplicacion = 10 * 5;
+console.log (multiplicacion);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+var div = 10 / 2;
+console.log (div);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+var div1 = 15 / 9;
+var resto = 15 - (div1 * 9);
+console.log (resto);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log( x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+let y = 10;
+let z = 5;
+let x = y * z;
+console.log( x);
